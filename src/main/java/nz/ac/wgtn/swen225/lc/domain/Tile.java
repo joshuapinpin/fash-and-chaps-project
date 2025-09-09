b7d2fafe51c53a98ec85a104c5d8094394e54d47
Hayley Far <farhayl@ecs.vuw.ac.nz>
2025-09-09 17:49:59 +1200

    feat(domain): add Tile and Position class skeletons
    Also adding and removing class files to fit what was planned as of last meeting

--- src/main/java/nz/ac/wgtn/swen225/lc/domain/Tile.java
+++ src/main/java/nz/ac/wgtn/swen225/lc/domain/Tile.java
@@ -2,8 +2,26 @@ package nz.ac.wgtn.swen225.lc.domain;
 
 /**
  * Represents a single tile in the maze
- * Each tile delegates behaviour to a TileAction using the Strategy Pattern
- * Tile holds the position on Maze and the current action/behaviour
+ * Each tile has a position and defines behaviour when a player steps on it
+ * Subclasses specify the type of tile (eg. wall, free, exit, info)
  */
-public class Tile {
+
+public abstract class Tile {
+    //Fields
+    Position pos;
+
+    //Constructor
+    public Tile(Position pos){}
+
+    //Getter for position
+    public Position pos(){return null;}
+
+    //Can player walk on the tile or is it blocked.
+    public boolean isAccessible(){return true;}
+
+    /*
+    * What happens when a player steps on tile
+    * eg. is it blocked? show message? trigger condition?
+    * */
+    public abstract void onEnter(Player player);
 }
