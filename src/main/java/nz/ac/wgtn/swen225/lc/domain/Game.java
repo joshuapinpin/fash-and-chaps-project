38f2aa599c27e1788d51588868351415ef67c0b0
Hayley Far <farhayl@ecs.vuw.ac.nz>
2025-09-09 17:49:59 +1200

    feat(domain): add Tile and Position class skeletons
    Also adding and removing class files to fit what was planned as of last meeting

--- src/main/java/nz/ac/wgtn/swen225/lc/domain/Game.java
+++
@@ -1,9 +0,0 @@
-package nz.ac.wgtn.swen225.lc.domain;
-
-/**
- * Class for managing game state and logic
- * Holding reference to the Maze and Player
- * Handles the players movements, tile interactions and other conditions
- */
-public class Game {
-}
--- src/main/java/nz/ac/wgtn/swen225/lc/domain/Position.java
+++ src/main/java/nz/ac/wgtn/swen225/lc/domain/Position.java
@@ -2,7 +2,22 @@ package nz.ac.wgtn.swen225.lc.domain;
 
 /**
  * Represents the coordinate position in the maze
- * Used by Player, Tiles and Maze to track the locations
+ * Holds x and y coordinate for the Tiles
  */
 public class Position {
+    //Fields
+    private int x;
+    private int y;
+
+    //Constructor
+    public Position(int x, int y){}
+
+    //Getters to get coordinates
+    public int x(){return 0;}
+    public int y(){return 0;}
+
+    //Setters to set coordinates
+    public void x(int i){}
+    public void y(int i){}
+
 }
