f154c4f6ac1c69eb4e4cfe355e78e301c0fd4ce9
Hayley Far <farhayl@ecs.vuw.ac.nz>
2025-09-12 16:50:59 +1200

    feat(domain): add Key class skeleton

--- src/main/java/nz/ac/wgtn/swen225/lc/domain/Key.java
+++ src/main/java/nz/ac/wgtn/swen225/lc/domain/Key.java
@@ -1,4 +1,22 @@
 package nz.ac.wgtn.swen225.lc.domain;
 
-public class Key {
+/**
+ * Represents a key entity that can be collected by player
+ */
+public class Key implements Entity {
+    //Field
+    String color;
+
+    //Constructor
+    Key(String color){throw new UnsupportedOperationException();}
+
+    //Player to collect key when stepping on its tile
+    public void onInteract(Player p){
+        throw new UnsupportedOperationException();
+    }
+
+    //Getter
+    public String getColor(){
+        throw new UnsupportedOperationException();
+    }
 }
