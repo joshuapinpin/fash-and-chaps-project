4c265182cbaede914b5194636115ba62ed7607ec
Hayley Far <farhayl@ecs.vuw.ac.nz>
2025-09-13 00:12:26 +1200

    feat(domain): add Info class skeleton

--- src/main/java/nz/ac/wgtn/swen225/lc/domain/Info.java
+++ src/main/java/nz/ac/wgtn/swen225/lc/domain/Info.java
@@ -1,4 +1,27 @@
 package nz.ac.wgtn.swen225.lc.domain;
 
-public class Info {
+/**
+ * Represents an info help tile
+ * Player stepping on this tile displays a message to the player
+ */
+public class Info extends Tile{
+    //Field
+    String message;
+
+    //Constructor
+    Info(String message, Position pos){
+        super(pos);
+        this.message = message;
+    }
+
+    //Help text to display when player enters this tile
+    @Override
+    public void onEnter(Player p){
+        throw new UnsupportedOperationException();
+    }
+
+    //Getter
+    public String getMessage(){
+        throw new UnsupportedOperationException();
+    }
 }
