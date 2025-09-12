85e28f649d769eb69c38138fc50909884f8ef3f8
Hayley Far <farhayl@ecs.vuw.ac.nz>
2025-09-13 00:41:06 +1200

    add Direction enum skeleton

--- src/main/java/nz/ac/wgtn/swen225/lc/domain/Direction.java
+++ src/main/java/nz/ac/wgtn/swen225/lc/domain/Direction.java
@@ -3,6 +3,22 @@ package nz.ac.wgtn.swen225.lc.domain;
 /**
  * Enum for the Player movement directions in the game
  * Having UP, DOWN, LEFT and RIGHT movements
+ *
+ * dx and dy fields represent how the Direction changes a Position
  */
 public enum Direction {
+    UP(0,-1),
+    DOWN(0,1),
+    LEFT(-1,0),
+    RIGHT(1,0);
+
+    private final int dx;
+    private final int dy;
+
+    //Constructor
+    Direction(int dx, int dy){throw new UnsupportedOperationException();}
+
+    //Getters, change in x and y
+    public int getDx(){throw new UnsupportedOperationException();}
+    public int getDy(){throw new UnsupportedOperationException();}
 }
