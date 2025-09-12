797cd9c912fd7d4e7e95e522387fa1d3b51f9e19
Hayley Far <farhayl@ecs.vuw.ac.nz>
2025-09-13 00:03:45 +1200

    feat(domain): add Exit class skeleton

--- src/main/java/nz/ac/wgtn/swen225/lc/domain/Exit.java
+++ src/main/java/nz/ac/wgtn/swen225/lc/domain/Exit.java
@@ -1,4 +1,21 @@
 package nz.ac.wgtn.swen225.lc.domain;
 
-public class Exit {
+/**
+ * Represents the exit tile of the level
+ * Player stepping on this tile finishes level or complete game
+ */
+public class Exit extends Tile{
+
+    //Potential field of GameController to trigger level complete?
+
+    //Constructor
+    Exit(Position pos){
+        super(pos);
+    }
+
+    //Player to finish level and move onto next or win game
+    @Override
+    public void onEnter(Player p){
+        throw new UnsupportedOperationException();
+    }
 }
