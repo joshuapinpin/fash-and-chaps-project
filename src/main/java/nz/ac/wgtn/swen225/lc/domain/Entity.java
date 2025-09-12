1689f54c39d59389926e880a02801f100aad7af5
Hayley Far <farhayl@ecs.vuw.ac.nz>
2025-09-13 00:29:10 +1200

    feat(domain): add Entity interface skeleton
    needing to edit Player class to no longer implement Entity as made it for interactable rather than moveable

--- src/main/java/nz/ac/wgtn/swen225/lc/domain/Entity.java
+++ src/main/java/nz/ac/wgtn/swen225/lc/domain/Entity.java
@@ -1,7 +1,12 @@
 package nz.ac.wgtn.swen225.lc.domain;
 
 /**
- * Base interface for movable entities (Player and enemies)
+ * Represents an interactable entity in the maze
+ * e.g. Key, Door, ExitLock and Treasure
+ * Each entity is to define what happens when a player interacts with it
  */
 public interface Entity {
+
+    //Called when a player interacts with this entity
+    void onInteract(Player player);
 }
