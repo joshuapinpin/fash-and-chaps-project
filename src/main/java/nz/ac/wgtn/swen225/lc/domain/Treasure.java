6fccdf619f26b7baa2dd310ccbf6be3f0f1553e4
Hayley Far <farhayl@ecs.vuw.ac.nz>
2025-09-12 23:47:19 +1200

    feat(domain): add Treasure class skeleton

--- src/main/java/nz/ac/wgtn/swen225/lc/domain/Treasure.java
+++ src/main/java/nz/ac/wgtn/swen225/lc/domain/Treasure.java
@@ -1,4 +1,11 @@
 package nz.ac.wgtn.swen225.lc.domain;
 
-public class Treasure {
+public class Treasure implements Entity{
+    //Constructor
+    Treasure(){throw new UnsupportedOperationException();}
+
+    //Player to collect and decrement treasuresLeft in player
+    public void onInteract(Player p){
+        throw new UnsupportedOperationException();
+    }
 }
