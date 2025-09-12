971707793fa810c33c4794099567763f5579a3bf
Hayley Far <farhayl@ecs.vuw.ac.nz>
2025-09-13 00:19:52 +1200

    feat(domain): add Free class skeleton

--- src/main/java/nz/ac/wgtn/swen225/lc/domain/Free.java
+++ src/main/java/nz/ac/wgtn/swen225/lc/domain/Free.java
@@ -1,4 +1,32 @@
 package nz.ac.wgtn.swen225.lc.domain;
 
-public class Free {
+import java.util.Optional;
+
+/**
+ * Represents a free tile that the player can walk on
+ * It may contain a collectable entity like a key, treasure, door etc
+ */
+public class Free extends Tile{
+    //Fields
+    Optional<Entity> collectable = Optional.empty();
+
+    //Constructor
+    Free(Position pos){
+        super(pos);
+    }
+
+    //Checks if collectable on tile to collect
+    @Override
+    public void onEnter(Player p){
+        throw new UnsupportedOperationException();
+    }
+
+    //Setter
+    public void setCollectable(Entity collectable){
+        throw new UnsupportedOperationException();
+    }
+    //Getter
+    public Optional<Entity> getCollectable(){
+        throw new UnsupportedOperationException();
+    }
 }
