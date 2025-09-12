e03c8d33929e06715bfbec01a5780ec6c3096a94
Hayley Far <farhayl@ecs.vuw.ac.nz>
2025-09-12 23:57:03 +1200

    feat(domain): add Wall class skeleton

--- src/main/java/nz/ac/wgtn/swen225/lc/domain/Wall.java
+++ src/main/java/nz/ac/wgtn/swen225/lc/domain/Wall.java
@@ -1,4 +1,19 @@
 package nz.ac.wgtn.swen225.lc.domain;
 
-public class Wall {
+public class Wall extends Tile{
+
+    //Constructor (does it have to be public?)
+    Wall(Position pos) {
+        super(pos);
+    }
+
+    //Only tile type you player cannot walk on so override
+    @Override
+    public boolean isAccessible(){return false;}
+
+    //Does nothing, player cannot go on Wall
+    @Override
+    public void onEnter(Player p){
+        throw new UnsupportedOperationException();
+    }
 }
