3f02c043b8de6702ea29e1dcfbc87f404d02762d
Hayley Far <farhayl@ecs.vuw.ac.nz>
2025-09-12 23:29:09 +1200

    feat(domain): add Door class skeleton

--- src/main/java/nz/ac/wgtn/swen225/lc/domain/Door.java
+++ src/main/java/nz/ac/wgtn/swen225/lc/domain/Door.java
@@ -1,4 +1,22 @@
 package nz.ac.wgtn.swen225.lc.domain;
 
-public class Door {
+public class Door implements Entity{
+    //Fields
+    String color;
+    boolean isOpen = false;
+
+    //Constructor
+    Door(String color){throw new UnsupportedOperationException();}
+
+    //Player to unlock door with correct key color (going through player inventory)
+    public void onInteract(Player p){
+        throw new UnsupportedOperationException();
+    }
+
+    //Getters
+    public String getColor(){throw new UnsupportedOperationException();}
+
+    public boolean isOpen() {
+        throw new UnsupportedOperationException();
+    }
 }
