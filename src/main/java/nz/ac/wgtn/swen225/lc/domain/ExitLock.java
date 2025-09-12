fb46128fb95f49d0e33b2818c5323deeebd91a1f
Hayley Far <farhayl@ecs.vuw.ac.nz>
2025-09-12 23:37:19 +1200

    feat(domain): add ExitLock class skeleton

--- src/main/java/nz/ac/wgtn/swen225/lc/domain/ExitLock.java
+++ src/main/java/nz/ac/wgtn/swen225/lc/domain/ExitLock.java
@@ -1,4 +1,15 @@
 package nz.ac.wgtn.swen225.lc.domain;
 
-public class ExitLock {
+public class ExitLock implements Entity{
+    //Fields
+    boolean isPassable = false;
+
+    //Constructor
+    ExitLock(){throw new UnsupportedOperationException();}
+
+    //Player to pass through once all treasures are collected in their inventory
+    public void onInteract(Player p){
+        throw new UnsupportedOperationException();
+    }
+
 }
