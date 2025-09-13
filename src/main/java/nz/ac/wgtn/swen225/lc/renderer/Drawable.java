52d2af1db8d528da1869636ecec4a22f306d25e3
Joshua Pinpin <pinpinjosh@ecs.vuw.ac.nz>
2025-09-13 18:04:18 +1200

    feat(app): added status and game panel.
    Both panels are put into a root panel.

--- src/main/java/nz/ac/wgtn/swen225/lc/renderer/Drawable.java
+++ src/main/java/nz/ac/wgtn/swen225/lc/renderer/Drawable.java
@@ -1,6 +1,7 @@
 package nz.ac.wgtn.swen225.lc.renderer;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Drawable {
     Map<Tile, Enum> allTiles = new HashMap<>();
@@ -9,7 +10,8 @@ public class Drawable {
     public void loadTiles(Map<Tile, Enum> allTiles){
 
     }
-
-
 }
+
+// Delete this later, i just needed it for compiling purposes - Josh
+interface Tile{}
 
