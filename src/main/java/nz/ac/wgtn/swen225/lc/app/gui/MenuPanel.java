3958649e9aa9a0a3ba3781fce22ac78299e352ed
Joshua Pinpin <pinpinjosh@ecs.vuw.ac.nz>
2025-09-16 02:38:01 +1200

    feat(app): implemented border layouts for the jpanels.
    Using border layouts, still needs work, may use something other than border layouts

--- src/main/java/nz/ac/wgtn/swen225/lc/app/gui/MenuPanel.java
+++ src/main/java/nz/ac/wgtn/swen225/lc/app/gui/MenuPanel.java
@@ -6,15 +6,13 @@ import java.awt.*;
 public class MenuPanel extends JPanel {
 
     // Size fields
-    public static final int PANEL_WIDTH = (GameWindow.WINDOW_HEIGHT / 4) * 2;
-    public static final int PANEL_HEIGHT = (GameWindow.WINDOW_HEIGHT / 4) * 3;;
-    public static final int PANEL_X = 0;
-    public static final int PANEL_Y = (GameWindow.WINDOW_HEIGHT - PANEL_HEIGHT) / 2;
+    public static final int PANEL_WIDTH = GameWindow.WINDOW_WIDTH;
+    public static final int PANEL_HEIGHT = GameWindow.WINDOW_HEIGHT / 8;
 
     private GameWindow window;
     MenuPanel(GameWindow window){
         this.window = window;
         setBackground(Color.green);
-        setBounds(PANEL_X+20,PANEL_Y,PANEL_WIDTH-20,PANEL_HEIGHT);
+        setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
     }
 }
---
+++ src/main/java/nz/ac/wgtn/swen225/lc/app/gui/TitlePanel.java
@@ -0,0 +1,15 @@
+package nz.ac.wgtn.swen225.lc.app.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class TitlePanel extends JPanel {
+    private GameWindow window;
+    public static int PANEL_WIDTH = GameWindow.WINDOW_WIDTH;
+    public static int PANEL_HEIGHT = GameWindow.WINDOW_HEIGHT / 8;
+    TitlePanel(GameWindow window){
+        this.window = window;
+        setBackground(Color.cyan);
+        setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
+    }
+}
