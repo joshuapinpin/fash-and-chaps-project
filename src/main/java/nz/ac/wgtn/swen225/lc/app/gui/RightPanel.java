2678bf621ea1e7ceba0f81a6dc07eb0fdba616f3
Joshua Pinpin <pinpinjosh@ecs.vuw.ac.nz>
2025-09-21 02:16:12 +1200

    feat(app): added utility class for creating buttons with custom background and tint when hovered and pressed.

--- src/main/java/nz/ac/wgtn/swen225/lc/app/gui/RightPanel.java
+++ src/main/java/nz/ac/wgtn/swen225/lc/app/gui/RightPanel.java
@@ -1,6 +1,7 @@
 package nz.ac.wgtn.swen225.lc.app.gui;
 
 import nz.ac.wgtn.swen225.lc.app.controller.GameController;
+import nz.ac.wgtn.swen225.lc.app.util.MyButton;
 import nz.ac.wgtn.swen225.lc.app.util.MyFont;
 import nz.ac.wgtn.swen225.lc.renderer.imgs.LoadingImg;
 
@@ -53,65 +54,11 @@ public class RightPanel extends JPanel implements ActionListener, ChangeListener
 
     }
     private void setupSingleButton(String name, Runnable action){
-        JButton button = new JButton(name){
-            private boolean hovered = false;
-            private boolean pressed = false;
-            {
-                addMouseListener(new java.awt.event.MouseAdapter() {
-                    @Override
-                    public void mouseEntered(java.awt.event.MouseEvent e) {
-                        hovered = true;
-                        repaint();
-                    }
-                    @Override
-                    public void mouseExited(java.awt.event.MouseEvent e) {
-                        hovered = false;
-                        pressed = false;
-                        repaint();
-                    }
-                    @Override
-                    public void mousePressed(java.awt.event.MouseEvent e) {
-                        pressed = true;
-                        repaint();
-                    }
-                    @Override
-                    public void mouseReleased(java.awt.event.MouseEvent e) {
-                        pressed = false;
-                        repaint();
-                    }
-                });
-            }
+        JButton button = MyButton.of(name, PANEL_WIDTH, PANEL_HEIGHT, 20,
+                LoadingImg.Sand.loadImage());
 
-            @Override
-            protected void paintComponent(Graphics g) {
-                int x = 0, y = 0;
-                while(x < PANEL_WIDTH){
-                    while(y < PANEL_HEIGHT){
-                        g.drawImage(LoadingImg.Sand.loadImage(), x, y,
-                                AppWindow.SQUARE_SIZE, AppWindow.SQUARE_SIZE, this);
-                        y += AppWindow.SQUARE_SIZE;
-                    }
-                    y = 0;
-                    x += AppWindow.SQUARE_SIZE;
-                }
-                if (hovered) {
-                    // Draw a semi-transparent black overlay for a darker tint
-                    g.setColor(new Color(0, 0, 0, 80)); // alpha 80 for subtle darkness
-                    g.fillRect(0, 0, getWidth(), getHeight());
-                }
-                if (pressed) {
-                    g.setColor(new Color(0, 0, 0, 140)); // even darker for pressed
-                    g.fillRect(0, 0, getWidth(), getHeight());
-                }
-                super.paintComponent(g);
-            }
-        };
-        button.setContentAreaFilled(false);
-        button.setOpaque(false);
-        button.addActionListener(this);
         button.setFocusable(false);
-        button.setFont(MyFont.PIXEL.getFont(20));
-        button.setForeground(Color.white);
+        button.addActionListener(this);
         buttonRunnableMap.put(button, action);
         add(button);
     }
