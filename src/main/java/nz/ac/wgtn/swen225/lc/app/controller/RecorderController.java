e4d2dfacf34743a9e9cd140b44dd288307d03f3a
Joshua Pinpin <pinpinjosh@ecs.vuw.ac.nz>
2025-09-26 02:26:49 +1200

    feat(app): removed implementation of recorder controller

--- src/main/java/nz/ac/wgtn/swen225/lc/app/controller/RecorderController.java
+++ src/main/java/nz/ac/wgtn/swen225/lc/app/controller/RecorderController.java
@@ -5,23 +5,4 @@ import nz.ac.wgtn.swen225.lc.recorder.Play;
 import nz.ac.wgtn.swen225.lc.recorder.Save;
 
 public class RecorderController {
-    Play play;
-    Save save;
-    GameController controller;
-
-    public RecorderController(GameController controller){
-        this.controller = controller;
-        play = new Play();
-        save = new Save();
-    }
-
-    public void addMovement(Input dir){
-        Save.addMovement(dir);
-    }
-
-    public void startRecording(){
-    }
-    public void stopRecording(){}
-    public void autoPlay(){}
-    public void stepByStep(){}
 }
