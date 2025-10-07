d69ad5740819050e25108d2f8618ad724da3f7f9
Thomas Ru <ruthom@ecs.vuw.ac.nz>
2025-10-07 17:59:47 +1300

    feat(persistency): Implement file loading and saving by users via Swing GUI.
    GamePersist and Persist act as facades to supply a simplified API for App to use.
    PersistManagers are essentially Strategies for how the persistency is implemented,
    e.g. GamePersistManager is specific to using JSON and Maze, but new concrete
    PersistManagers could easily be added to implement persistency via XML or binary for example.
    
    Note that the pom.xml file was edited to disable assertions during testing, since
    JaCoCo considers them valid instructions that ran (artificially increasing the instruction coverage).
    Unfortunately though, JaCoCo still considers them valid instructions even after disabling, but
    at least now they're considered instructions that didn't run (artificially lowering the instruction
    coverage), and we get an underestimate rather than overestimate of our coverage.

---
+++ src/main/java/nz/ac/wgtn/swen225/lc/persistency/save/util/SwingFileDialog.java
@@ -0,0 +1,81 @@
+package nz.ac.wgtn.swen225.lc.persistency.save.util;
+
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.File;
+import java.util.Optional;
+
+/**
+ * Concrete file saving/loading dialogs, implemented using Swing.
+ * @author Thomas Ru - 300658840
+ */
+public class SwingFileDialog implements FileDialog {
+
+    /**
+     * Show a dialog for loading a file.
+     * @param parent - the parent JFrame/window.
+     * @param extension - the file extension, as a String.
+     * @return - an Optional<File>, which will be empty if no file was selected.
+     */
+    @Override
+    public Optional<File> showOpenDialog(JFrame parent, String extension) {
+        JFileChooser chooser = new JFileChooser();
+        chooser.setFileFilter(new FileNameExtensionFilter(
+                extension.toUpperCase() + " files", extension
+        ));
+
+        int result = chooser.showOpenDialog(parent);
+        if (result == JFileChooser.APPROVE_OPTION) {
+            return Optional.of(chooser.getSelectedFile());
+        }
+        return Optional.empty();
+    }
+
+    /**
+     * Show a save dialog with the given default file name.
+     * The defaultName can be null or empty, in which case the chooser starts empty.
+     * @param parent - the parent JFrame/window.
+     * @param defaultName - the default name of the file to save, as a String.
+     * @param extension - the file extension, as a String.
+     */
+    @Override
+    public Optional<File> showSaveDialog(JFrame parent, String defaultName, String extension) {
+        // get user to select a file to save to
+        JFileChooser chooser = new JFileChooser();
+        if (defaultName != null && !defaultName.isBlank()) {
+            chooser.setSelectedFile(new File(defaultName));
+        }
+        chooser.setFileFilter(new FileNameExtensionFilter(
+                extension.toUpperCase() + " files", extension
+        ));
+        int result = chooser.showSaveDialog(parent);
+        if (result != JFileChooser.APPROVE_OPTION) {
+            return Optional.empty();
+        }
+
+        // tidy up the chosen file
+        File file = chooser.getSelectedFile();
+        if (!file.getName().toLowerCase().endsWith("." + extension.toLowerCase())) {
+            file = new File(file.getAbsolutePath() + "." + extension);
+        }
+        if (file.exists() &&  confirmOverwrite(parent)) {
+            return Optional.empty();
+        }
+
+        return Optional.of(file);
+    }
+
+    /**
+     * Confirms whether a user wants to overwrite a file.
+     * @param parent - the parent JFrame/window.
+     * @return - true if they do, otherwise false.
+     */
+    private boolean confirmOverwrite(JFrame parent) {
+        int overwrite = JOptionPane.showConfirmDialog(parent,
+                "File already exists. Overwrite?",
+                "Confirm Save",
+                JOptionPane.YES_NO_OPTION,
+                JOptionPane.WARNING_MESSAGE);
+        return overwrite == JOptionPane.YES_OPTION;
+    }
+}
